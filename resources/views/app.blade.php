b2a5fa6229969823c6f68a4b4e05aec2af10cd0f
NCapurro <nicolascapurro23@gmail.com>
2026-02-22 21:56:14 -0300

    no translate

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -1,7 +1,10 @@
 <!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<!--<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">-->
+    <html lang="es">
     <head>
+        
         <meta charset="utf-8">
+        <meta name="google" content="notranslate">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         
         <title inertia>{{ config('app.name', 'Laravel') }}</title>
